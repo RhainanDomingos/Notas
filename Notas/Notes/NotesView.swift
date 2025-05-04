6fbf4e3a3af25d0f7002974c6aca554bee679c54
Rhainan Domingos <rhainandomingos@icloud.com>
2025-05-03 23:13:57 -0300

    Create app Notas with complete Login Screen

---
+++ Notas/Notes/NotesView.swift
@@ -0,0 +1,18 @@
+//
+//  NotesView.swift
+//  Notas
+//
+//  Created by Rhainan Domingos on 03/05/25.
+//
+
+import SwiftUI
+
+struct NotesView: View {
+    var body: some View {
+        Text("Notes View")
+    }
+}
+
+#Preview {
+    NotesView()
+}
---
+++ Notas/Register/RegisterView.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterView.swift
+//  Notas
+//
+//  Created by Rhainan Domingos on 03/05/25.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        Text("Register View")
+    }
+}
+
+#Preview {
+    RegisterView()
+}
