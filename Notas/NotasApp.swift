e61f2047ad68135fb9b9f25ce0c39a114f89575b
Rhainan Domingos <rhainandomingos@icloud.com>
2025-05-03 21:54:13 -0300

    Initial Commit

---
+++ Notas/NotasApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotasApp.swift
+//  Notas
+//
+//  Created by Rhainan Domingos on 03/05/25.
+//
+
+import SwiftUI
+
+@main
+struct NotasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
